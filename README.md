fb56044386d457975a6387dcdab8955375570ad9
Omid Hezaveh <a2gik.fi>
2016-06-22 09:23:27 +0300

    make components

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-# sayitlikeme
+# sayitlike.me
 
 ## User stories
   * map url/username to pages for each user
+
+## Components
+  * Login component 
+  * Add recording
+  * Show user component
+  * Search Component
+## Data structure
+  * User
+    * twitter handle
+    * recording address 
+    * name
+    * name clearification
+    * perffered name 
+    * date
+    * lastUpdated
+{
+    "twitterHandle": "omidfi",
+    "recordingAddress": "/recordings/omidfi/sound.mp3",
+    "name": "Omid Hezaveh",
+    "nameClarification": "Omeeed  Heeeezaaaaveeeeh",
+    "preffered name": "Omid",
+    "date": "2016-06-22",
+    "lastUpdated": "23423423423423"
+}
--- app/index.js
+++ app/index.js
@@ -6,9 +6,34 @@ const App = React.createClass({
         return <div>
                   <h1> Say It Like Me! </h1>
                   <Description description="I know how it's pronounced!"/>
+                  <Search/>
+                  <Login />
+                  <User />
+                  <Recorder />
                </div>;
     }
 });
+
+
+const Search = (props) => <div>
+                              <h2>Search:</h2>
+                              <input type="text"/>
+                              <button>Find</button>
+                          </div>;
+const Login = (props) => <div>
+                             <h2>Login With Twitter </h2>
+                             <button> Login </button>
+                         </div>;
+const User = (props) => <div>
+                            <h1>My name is ... </h1>
+                            <p>Please call me like this</p>
+                            <span>Play</span>
+                        </div>;
+const Recorder = (props) => <div>
+                                <h1> Record the name </h1>
+                                <div>Record</div>
+                            </div>
+
 
 const Description = (props) => <div> {props.description} </div>;
 ReactDom.render(<App/>, window.document.getElementById('target'));
