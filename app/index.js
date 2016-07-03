177dc26e63809ccd28d54a97b226d3ff25dd2803
Omid Hezaveh <a@gmik.fi>
2016-07-03 10:09:47 +0300

    add updating user info by ajax resp

--- app/index.js
+++ app/index.js
@@ -5,27 +5,37 @@ const axios = require('axios');
 const App = React.createClass({
     getInitialState() {
         return {
-        twitterId: ''
+            userInfo : {
+                twitterId: '',
+                name: '',
+                nameClarification: ''
+            }
         }
+    },
+    showUser(response) {
+        this.setState({
+            userInfo : {
+                twitterId: response.twitterId,
+                name: response.name,
+                nameClarification: response.nameClarification 
+            }
+        });
     },
     componentDidMount() {
         axios.get('http://localhost:8000/omidfi')
              .then( data => {
-                 const response = data;
-                 console.log();
-                 window.myData = data;
-                 this.setState({
-                     twitterId: response.data[0].twitterId
-                 });
+                 console.log('response is back', data);
+                 this.showUser(data.data[0]);
              });
     },
     render() {
         return <div>
-                  <h1> Say It Like Me! {this.state.twitterId}</h1>
+                  {console.log('this.state', this.state)}
+                  <h1> Say It Like Me!</h1>
                   <Description description="I know how it's pronounced!"/>
+                  <User userInfo={this.state.userInfo}/>
                   <Search/>
                   <Login />
-                  <User />
                   <Recorder />
                </div>;
     }
@@ -42,8 +52,10 @@ const Login = (props) => <div>
                              <button> Login </button>
                          </div>;
 const User = (props) => <div>
-                            <h1>My name is  {this.props.twitterId}</h1>
-                            <p>Please call me like this</p>
+                            {console.log('user props.userInfo', props.userInfo)}
+                            <h1>My name is  <b> {props.userInfo.name}</b></h1>
+                            <h2>My twitter handle is  <b> {props.userInfo.twitterId}</b></h2>
+                            <p>Please call me like this: {props.userInfo.nameClarification}</p>
                             <span>Play</span>
                         </div>;
 const Recorder = (props) => <div>
