cea932cebba550fafb55bf9bc0eb3eecc4cdd978
Omid Hezaveh <a@gik.fi>
2016-07-18 12:01:40 +0300

    add secrets.default.js and gitignore secrets.js
    So that private keys and passwords wont be saved in github

---
+++ config/secrets.default.js
@@ -0,0 +1,12 @@
+// config/secret.js
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'twitterAuth' : {
+        'consumerKey'       : '',
+        'consumerSecret'    : '',
+        'callbackURL'       : ''
+    }
+
+
+};
