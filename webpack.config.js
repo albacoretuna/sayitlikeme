c1dd8175d201a1e57ca797e9cd6bc7ac20ced800
Omid Hezaveh <a@gik.fi>
2016-07-19 10:54:34 +0300

    Add eslint, remove jshint

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,11 @@ module.exports = {
 module: {
   loaders: [
     {
-      test: /\.js$/,
+      test: /\.jsx?$/,
       exclude: /(node_modules)/,
       loader: 'babel', // 'babel-loader' is also a legal name to reference
       query: {
-        presets: ['react', 'es2015']
+        presets: ['es2015','react']
       }
     }
   ]
