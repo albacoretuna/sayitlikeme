4d8d228e673233c597ff6dd5b8550895ca449163
Omid Hezaveh <a@gik.fi>
2016-07-27 16:45:22 +0300

    Style menu

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -4,9 +4,21 @@ const Navigation = React.createClass({
     render() {
         return (
             <div>
-                <ul className="navigation">
-                    <li><Link to="/home-"> Homepage </Link></li>
-                    <li><Link to="/add-"> Register </Link></li>
+                <ul className="navigation-menue">
+                    <li className="navigation-menue-item">
+                        <Link to="/home-"
+                            className="navigation-menue-link"
+                            activeClassName="navigation-menue-active">
+                            Homepage
+                        </Link>
+                    </li>
+                    <li className="navigation-menue-item">
+                        <Link to="/add-"
+                            className="navigation-menue-link"
+                            activeClassName="navigation-menue-active">
+                            Register
+                        </Link>
+                    </li>
                 </ul>
             </div>
         );
