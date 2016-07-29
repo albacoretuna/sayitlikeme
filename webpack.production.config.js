863fd1713bd551f26e2f1c04b43b1891179a61d3
Omid <a@gik.fi>
2016-07-29 00:36:42 -0400

    Remove livereload from production webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,5 +1,4 @@
 /* globals __dirname */
-const LiveReloadPlugin = require('webpack-livereload-plugin');
 const webpack = require('webpack');
 module.exports = {
     devtool: 'cheap-module-source-map',
@@ -22,7 +21,6 @@ module.exports = {
         ]
     },
     plugins: [
-        new LiveReloadPlugin(),
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
