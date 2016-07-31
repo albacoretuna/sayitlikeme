81f3275d1213d17e3f59b5bc06888f750d61ba85
Omid Hezaveh <a@gik.fi>
2016-07-31 11:36:12 +0300

    Clean up server.js comment console.logs

--- server/server.js
+++ server/server.js
@@ -40,35 +40,36 @@ passport.use(new TwitterStrategy({
     consumerSecret: secrets.twitterAuth.consumerSecret,
     callbackURL: secrets.twitterAuth.callbackURL
 },
-    function(token, tokenSecret, profile, cb) {
-        User.findOne({ twitterId: profile.username }, function (err, user) {
-            if(err) {
-                logger.log('error', 'Twitter strategy problem', {err: err});
-            }
-            if(user) {
-                return cb(err, user);
-            } else {
-                let newUser = new User({ twitterId: profile.username });
-                newUser.save(function(err){
-                    if(err) {
-                        return err;
-                    } else {
-                        return cb(err, newUser);
-                    }
-                });
-            }
-        });
-    })
+function(token, tokenSecret, profile, cb) {
+    User.findOne({ twitterId: profile.username }, function (err, user) {
+        if(err) {
+            logger.log('error', 'Twitter strategy problem', {err: err});
+        }
+        if(user) {
+            return cb(err, user);
+        } else {
+            let newUser = new User({ twitterId: profile.username });
+            newUser.save(function(err){
+                if(err) {
+                    logger.log('error', 'new user not saved', {err: err});
+                    return err;
+                } else {
+                    return cb(err, newUser);
+                }
+            });
+        }
+    });
+})
 );
 
 
 passport.serializeUser(function(user, done) {
-    logger.log('info', 'user id serializsed as: ', {id: user.twitterId});
+    // logger.log('info', 'user id serializsed as: ', {id: user.twitterId});
     done(null, user.twitterId);
 });
 
 passport.deserializeUser(function(obj, done) {
-    logger.log('info', 'user deserializsed as: ', {user: obj});
+    // logger.log('info', 'user deserializsed as: ', {user: obj});
     done(null, obj);
 });
 // parsing, and session handling.
@@ -82,14 +83,14 @@ let sessionOptions = {
     cookie: {maxAge: 1000 * 60 * 20}
 };
 
-// in production enable secure cookie and set domain, also nginx needs 
+// in production enable secure cookie and set domain, also nginx needs
 // proxy_set_header X-Forwarded-Proto $scheme;
 if (app.get('env') === 'production') {
-	sessionOptions.name = 'saySessionId';
-	sessionOptions.proxy = true;
-	app.set('trust proxy', 'loopback'); 
-	sessionOptions.cookie.domain = 'sayitlike.me'; 
-	sessionOptions.cookie.secure = true; 
+    sessionOptions.name = 'saySessionId';
+    sessionOptions.proxy = true;
+    app.set('trust proxy', 'loopback');
+    sessionOptions.cookie.domain = 'sayitlike.me';
+    sessionOptions.cookie.secure = true;
 }
 
 app.use(session(sessionOptions));
