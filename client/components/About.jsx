ddc39c797111f68fb724763edabf4164a10c4083
Omid Hezaveh <a@gik.fi>
2016-08-02 08:00:32 +0300

    Fix typos

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -6,12 +6,12 @@ const About = () => <div>
         Usually people don't get my family name in first few tries as it's not a common one. Even though I have learned to not worry about that too much, still I feel bad when I see someone struggling to pronounce my name.
     </p>
     <h4>2. Listening to Lots of Podcasts</h4>
-    <p> I like listening to podcasts. One of my favorites is <a href="https://changelog.com/" target="_blank"> The Changelog</a>. It's very common to hear the hosts of The Changelog,  Adam and Jerod,  guessing,  pronouncing, and then apologizing for mispronounciation of the names. But one time I actually heard them struggling with myown family name. It was then that I really hoped I could do something about it.
+    <p> I like listening to podcasts. One of my favorites is <a href="https://changelog.com/" target="_blank"> The Changelog</a>. It's very common to hear the hosts of The Changelog,  Adam and Jerod,  guessing,  pronouncing, and then apologizing for mispronounciation of the names. But one time I actually heard them struggling with my own family name. It was then that I really hoped I could do something about it.
     </p>
     <h4>3. Idea  </h4>
     <p> The idea is simply asking people to log in by twitter and <strong>record the pronounciation of their names. </strong>So that others can find the recordings and listen to the right pronounciation to get the name more easily. </p>
     <h4>4. Implementation </h4>
-    <p>Being mere mortals, we don't alwasy get the chance to bring our ideas to life. But this time I was lucky and during my commutes and summer vacation it was possible to make a betha version of Sayitlike.me </p>
+    <p>Being mere mortals, we don't always get the chance to bring our ideas to life. But this time I was lucky and during my commutes and summer vacation it was possible to make a betha version of Sayitlike.me </p>
     <p>It's <strong>free</strong> and <strong>open source</strong> hoping that others will help to improve it. All the code is on github: <a href="https://github.com/omidfi/sayitlikeme">Link</a> </p>
     <p> Feel free to say hi on twitter: <a href="https://twitter.com/omidfi">@omidfi</a></p>
 
