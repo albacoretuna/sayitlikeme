3231fafc929d2c39fa43062b412a84f0885ce962
Jerod Santo <jerod.santo@gmail.com>
2016-08-01 20:52:39 -0500

    Fix typos in the About page

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -3,13 +3,13 @@ const About = () => <div>
     <h1>Why this?</h1>
     <h4> 1. Living With an Uncommon Name </h4>
     <p>
-        Usually people don't get my family name in first few tries as it's not a common one. Even though I have learnt to not worry about that too much, still I feel bad when I see someone struggling to pronounce my name.
+        Usually people don't get my family name in first few tries as it's not a common one. Even though I have learned to not worry about that too much, still I feel bad when I see someone struggling to pronounce my name.
     </p>
-    <h4>2. Listenning to Lots of Podcasts</h4>
-    <p> I like listenning to podcasts. One of my favorites is <a href="https://changelog.com/" target="_blank"> The Change Log</a>. It's very common to hear the hosts of The Changelog,  Adam and Jerod,  guessing,  pronouncing, and then apologizing for mispronounciation of the names. But one time I actually heard them struggling with myown family name. It was then that I really hoped I could do something about it.
+    <h4>2. Listening to Lots of Podcasts</h4>
+    <p> I like listening to podcasts. One of my favorites is <a href="https://changelog.com/" target="_blank"> The Changelog</a>. It's very common to hear the hosts of The Changelog,  Adam and Jerod,  guessing,  pronouncing, and then apologizing for mispronounciation of the names. But one time I actually heard them struggling with myown family name. It was then that I really hoped I could do something about it.
     </p>
     <h4>3. Idea  </h4>
-    <p> The idea is simply asking people to login by twitter and <strong>record the pronounciation of their names. </strong>So that others can find the recordings and listen to the right pronounciation to get the name more easily. </p>
+    <p> The idea is simply asking people to log in by twitter and <strong>record the pronounciation of their names. </strong>So that others can find the recordings and listen to the right pronounciation to get the name more easily. </p>
     <h4>4. Implementation </h4>
     <p>Being mere mortals, we don't alwasy get the chance to bring our ideas to life. But this time I was lucky and during my commutes and summer vacation it was possible to make a betha version of Sayitlike.me </p>
     <p>It's <strong>free</strong> and <strong>open source</strong> hoping that others will help to improve it. All the code is on github: <a href="https://github.com/omidfi/sayitlikeme">Link</a> </p>
