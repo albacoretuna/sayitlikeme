06ab5a7936b53e427279a76d45009253bee6ce86
Sallar Kaboli <sallar.kaboli@gmail.com>
2016-08-02 12:18:55 +0300

    Readme and package.json cleanup

---
+++ server/config/secrets.js
@@ -0,0 +1,18 @@
+// config/secret.js
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'twitterAuth' : {
+        'consumerKey'       : '',
+        'consumerSecret'    : '',
+        'callbackURL'       : ''
+    },
+    'session' : {
+        'secret': ''
+    },
+    'db' : {
+        'dbName' : '',
+        'ongoHost': ''
+    }
+
+};
