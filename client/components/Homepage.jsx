55575b2b6afaaadbe13821f07c834f8efaa5ad38
Omid Hezaveh <a@gik.fi>
2016-08-06 08:03:20 +0300

    Restyle search improve manifest improve footer

--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Search from './Search.jsx';
 import { Link } from 'react-router';
 const Homepage = () => <div>
-    <h1> Say It Like Me! </h1>
+     <h1> <img src="img/logo-no-text.svg" className="logo"/>Say It Like Me! </h1>
     <h4> A <strong>free</strong> and <strong>open source</strong> service to record the pronunciation of your name, to help others pronounce it right!</h4>
     <Link to='add-' className="button button-primary"> Register </Link>
     <h4>Record Your name now!  </h4>
