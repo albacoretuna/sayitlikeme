cefec3e74385e8b5b7a9cd80830ab74e298047d2
Omid Hezaveh <a@gik.fi>
2016-08-09 23:22:50 +0300

    Add Playing... text while audio's playing, add preload

--- client/components/AudioPlayer.jsx
+++ client/components/AudioPlayer.jsx
@@ -1,27 +1,44 @@
 import React from 'react';
 
-const AudioPlayer = (props) => {
-    const play = (event) => {
+const AudioPlayer = React.createClass({
+    getInitialState() {
+        return {
+            playing : false
+        };
+    },
+    play(event) {
         event.preventDefault();
         let audio = event.target.firstChild;
-        audio.play();
-    };
-    return (
-        <div>
-            <h4> Listen to how I pronounce it:&nbsp;
-            <a href="" className="audio-player-play" onClick={play}>
-                <audio>
-                    Your browser does not support the <code>audio</code> element.
-                    <source src={`./audio-upload/${props.filename}.mp3`} type="audio/mpeg" />
-                    <source src={`./audio-upload/${props.filename}.ogg`} type="audio/ogg" />
-                    <source src={`./audio-upload/${props.filename}.wav`} type="audio/wav" />
-                </audio>
-                <strong> ▸ </strong>&nbsp; play
-            </a>
-        </h4>
-        </div>
-    );
-};
+        if(audio.paused) {
+            audio.play();
+            this.setState({
+                playing : true
+            });
+        }
+        audio.addEventListener('ended',() => {
+            this.setState({
+                playing : false
+            });
+        });
+    },
+    render() {
+        return (
+            <div>
+                <h4> Listen to how I pronounce it:&nbsp;
+                    <a href="" className="audio-player-play" onClick={this.play}>
+                        <audio preload="auto">
+                            Your browser does not support the <code>audio</code> element.
+                            <source src={`./audio-upload/${this.props.filename}.mp3`} type="audio/mpeg" />
+                            <source src={`./audio-upload/${this.props.filename}.ogg`} type="audio/ogg" />
+                            <source src={`./audio-upload/${this.props.filename}.wav`} type="audio/wav" />
+                        </audio>
+                        {this.state.playing ? 'playing...' : 'play' }
+                    </a>
+                </h4>
+            </div>
+        );
+    }
+});
 AudioPlayer.propTypes = {
     filename: React.PropTypes.string,
 };
