b2677db0bfd708a2557a4f94702bd4ddabaee774
Omid Hezaveh <a@gik.fi>
2016-08-09 23:02:21 +0300

    Remove unused Recorder component

--- client/components/Recorder.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-const Recorder = (props) => <div>
-                                <h1> Record the name </h1>
-                                <div>Record</div>
-                            </div>;
-
-export default Recorder;
